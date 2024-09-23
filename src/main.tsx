04d2b0cea9333b29e32216a8d9474e39d17473b9
koujidev <atabek122002@gmail.com>
2024-09-23 11:51:18 +0500

    Added @vercel/analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { SnackbarProvider } from 'notistack';
+import { Analytics } from '@vercel/analytics/react';
 
 import { App } from './App';
 
@@ -14,6 +15,7 @@ createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
       <SnackbarProvider maxSnack={1} dense autoHideDuration={2000}>
         <App />
+        <Analytics />
       </SnackbarProvider>
     </Provider>
   </Router>,
